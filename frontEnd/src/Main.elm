ca4cbcc08b757344e702b69b8b14a22b6e8dad50
Abdul Rehman Liaqat <liaqat@uni-hildesheim.de>
2019-03-23 14:39:28 +0100

    working http

--- frontEnd/src/Main.elm
+++ frontEnd/src/Main.elm
@@ -3,13 +3,21 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput, onClick)
 
+import Browser
+import Html exposing (Html, text, pre)
+import Http
 
 
 -- MAIN
 
 
 main =
-  Browser.sandbox { init = init, update = update, view = view }
+  Browser.element
+    { init = init
+    , update = update
+    , subscriptions = subscriptions
+    , view = view
+    }
 
 
 
@@ -22,9 +30,9 @@ type alias Model =
   }
 
 
-init : Model
-init =
-  Model "" ""
+init : () -> (Model, Cmd Msg)
+init a =
+  (Model "" "",Cmd.none)
 
 
 
@@ -34,13 +42,24 @@ init =
 type Msg
   = Name String |  Submit
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> (Model,Cmd Msg)
 update msg model =
   case msg of
     Name name ->
-      { model | name = name }
+      ({ model | name = name },Cmd.none)
     Submit ->
-       {model | result = model.name}
+      ({model | result = model.name},Cmd.none)
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
+
+
 
 
 -- VIEW
